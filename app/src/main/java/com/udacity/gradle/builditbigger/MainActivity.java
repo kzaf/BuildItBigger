68137e2b40385d789077c2e07ba5a4a3c35101b4
Konstantinos Zafeiropoulos <k.a.zafeiropoulos@gmail.com>
2019-06-21 03:36:14 +0300

    -Create a new Gradle Java project -Introduce a project dependency between your app and the new Java Library -Make the button display a toast showing a joke retrieved from your Java joke telling library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.zaf.javalib.JavaJokerClass;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        JavaJokerClass javaJokerClass = new JavaJokerClass();
+
+        Toast.makeText(this, javaJokerClass.JokeFromJavaLib(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javaLib/src/main/java/com/zaf/javalib/JavaJokerClass.java
@@ -0,0 +1,7 @@
+package com.zaf.javalib;
+
+public class JavaJokerClass {
+    public String JokeFromJavaLib(){
+        return "Why did the chicken cross the road?";
+    }
+}
